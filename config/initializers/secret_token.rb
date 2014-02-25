25e6e89e7ffaa33e3fb8e2b16c9e54e7cea05471
vagrant <vagrant@student-dev-box>
2014-02-25 16:01:11 +0000

    Initial commit for my first rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Metube::Application.config.secret_key_base = '62f516b82d6ffd396ad44d8ab2c9969ab6afffb9b1ab91e3c5191cd671850490e16df0cf6af8c555b23ce92d7ff371bc41f808bdde720eb38efc8ac0a20eb32a'
